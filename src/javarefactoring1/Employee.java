62989a4c510c8fa63e4821c42786f11e889070cf
zzaier <zaier.zied@uqam.ca>
2017-09-30 09:54:18 -0400

    Package change Commit

--- src/javarefactoring1/Employee.java
+++ src/javarefactoring1/Employee.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+package javarefactoring1;
 
 public class Employee {
    
--- src/javarefactoring1/PartTime.java
+++ src/javarefactoring1/PartTime.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package javarefactoring1;
 /**
  *
  * @author zzaier
