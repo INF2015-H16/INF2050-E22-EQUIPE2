15397c2d2b8ac73f47c821a40618bdc644a0c2bf
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-19 17:47:05 -0400

    Classe Lotissement avec ses attributs.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Lotissement.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 1
+ *
+ * Terrain : Cette classe permet de modéliser le lotissement.
+ *
+ * @author Achou Henri Joël / Akaffou
+ * @version 19 mai 2022
+ *
+ */
+public class Lotissement {
+    
+    //Déclaration des variables
+    private String description;
+    private int nombreDroitPassage;
+    private int nombreService;
+    private int superficie;
+    private String dateMesure;
+    
+}
