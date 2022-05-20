5fce1c49f14459c28158a7687b026c2adfcdb4c8
mtljason322 <jordanmone@gmail.com>
2022-05-20 18:03:26 -0400

    Initial commit

---
+++ src/inf2050/e22/equipe2/INF2050E22EQUIPE2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ *
+ * @author akaff
+ */
+public class INF2050E22EQUIPE2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ceci est un test de push");
+        
+        System.out.println("Ceci est un 2eme test");
+    }
+    
+}
