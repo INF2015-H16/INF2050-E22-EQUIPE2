5c25dc7582b8c95eb82f0d6e4cead5ade4ceea25
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-24 20:07:00 -0400

    Méthode pour lire les données à partir du fichier json.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationTerrain.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationTerrain.java
@@ -64,6 +64,23 @@ public class EvaluationTerrain {
     
     private static int taille;
 
+    private int idTerrain;
+    private double prixMininimum;
+    private double prixMaximum;
+    private double montantTerrain;
+    private double montantTaxeScolaire;
+    private double montantTaxeMunicipale;
+
+    private double [] montantsLot;
+    private double [] montantsPass;
+    private double [] montantsServ;
+    private double [] montantsParLot;
+    private String [] descriptions;
+    private int [] passages;
+    private int [] services;
+    private int [] superficies;
+    private String [] dates;
+
     /**
      * Lire les données à partir du fichier entree.json et en extraire
      * les détails des valeurs des différents lots et créer des objets Terrain.
@@ -134,5 +151,338 @@ public class EvaluationTerrain {
         }
 
     }  
-    
+   
+    /**
+     * Obtenir l'identifiant du terrain.
+     *
+     */
+    public void obtenirTypeTerrain() throws IOException, NullPointerException {
+        int terr;
+
+        terr = terrain.getTypeTerrain();
+
+        idTerrain = terr;
+    }
+
+    /**
+     * Obtenir le prix minimum du terrain.
+     *
+     */
+    public void obtenirPrixMin() throws IOException, NullPointerException {
+        String pMin;
+        double prixMin;
+
+        pMin = terrain.getPrixMin();
+        String prixMn = pMin.replaceAll(DECIMAL_ONLY, "");
+        prixMin = Double.parseDouble(prixMn);
+
+        prixMininimum = prixMin;
+
+    }
+
+    /**
+     * Obtenir le prix maximum du terrain.
+     *
+     */
+    public void obtenirPrixMax() throws IOException, NullPointerException {
+        String pMax;
+        double prixMax;
+
+        pMax = terrain.getPrixMax();
+        String priMx = pMax.replaceAll(DECIMAL_ONLY, "");
+        prixMax = Double.parseDouble(priMx);
+
+        prixMaximum = prixMax;
+
+    }
+
+    /**
+     * Obtenir la description des différents lots qui composent le terrain.
+     *
+     */
+    public void obtenirDescription() throws IOException, NullPointerException {
+        String desc;
+
+        descriptions = new String[taille];
+
+        for (int i = 0; i < taille; i++) {
+            desc = lotissements.get(i).getDescription();
+
+            descriptions[i] = desc;
+        }
+    }
+
+    /**
+     * Obtenir le nombre de droits de passage de chaque lot
+     * du terrain.
+     *
+     */
+    public void obtenirNbreDroitPassage() throws IOException,
+            NullPointerException {
+        int pass;
+
+        passages = new int[taille];
+
+        for (int i = 0; i < taille; i++) {
+            pass = lotissements.get(i).getNombreDroitPassage();
+
+            passages[i] = pass;
+
+        }
+    }
+
+    /**
+     * Obtenir le nombre de services de chaque lot
+     * du terrain.
+     * 
+     */
+    public void obtenirNbreService() throws IOException, NullPointerException {
+        int serv;
+
+        services = new int[taille];
+
+        for (int i = 0; i < taille; i++) {
+            serv = lotissements.get(i).getNombreService();
+
+            services[i] = serv;
+
+        }
+    }
+
+    /**
+     * Obtenir la superficie de chaque lot
+     * du terrain.
+     */
+    public void obtenirSuperficie() throws IOException, NullPointerException {
+        int sup;
+
+        superficies = new int[taille];
+
+        for (int i = 0; i < taille; i++) {
+            sup = lotissements.get(i).getSuperficie();
+
+            superficies[i] = sup;
+
+        }
+    }
+
+    /**
+     * Obtenir la date de chaque lot
+     * du terrain.
+     */
+    public void obtenirDateMesure() throws IOException, NullPointerException {
+        String mes;
+
+        dates = new String[taille];
+
+        for (int i = 0; i < taille; i++) {
+            mes = lotissements.get(i).getDateMesure();
+
+            dates[i] = mes;
+
+        }
+    }
+
+    /**
+     * Calculer le montant de chaque lot  de chaque lot
+     * du terrain.
+     * 
+     */
+    public void calculerMontantLot() throws IOException, NullPointerException {
+        double mntLot = 0.0;
+
+        montantsLot = new double[taille];
+
+        for (int i = 0; i < taille; i++) {
+            switch (idTerrain) {
+                case Terrain.TERRAIN_AGRICOLE: {
+                    mntLot = superficies[i] * prixMininimum;
+                }
+                break;
+                case Terrain.TERRAIN_RESIDENTIEL: {
+                    mntLot = superficies[i] *
+                            ((prixMininimum + prixMaximum)/2);
+                }
+                break;
+                case Terrain.TERRAIN_COMMERCIAL: {
+                    mntLot = superficies[i] * prixMaximum;
+                }
+                break;
+                default:
+                    break;
+            }
+
+            montantsLot[i] = mntLot;
+
+        }
+    }
+
+    /**
+     * Calculer le montant des droits de passage.
+     *
+     */
+    public void calculerDroitPassage() throws IOException,
+            NullPointerException {
+        double mntDroit = 0.0;
+
+        montantsPass = new double[taille];
+
+        for (int i = 0; i < taille; i++) {
+            switch (idTerrain) {
+                case Terrain.TERRAIN_AGRICOLE: {
+                    mntDroit = 500 - (passages[i] * (0.05 * montantsLot[i]));
+                }
+                break;
+                case Terrain.TERRAIN_RESIDENTIEL: {
+                    mntDroit = 500 - (passages[i] * (0.10 * montantsLot[i]));
+                }
+                break;
+                case Terrain.TERRAIN_COMMERCIAL: {
+                    mntDroit = 500 - (passages[i] * (0.15 * montantsLot[i]));
+                }
+                break;
+                default:
+                    break;
+            }
+
+            montantsPass[i] = mntDroit;
+
+        }
+    }
+
+    /**
+     * Calculer le montant des services.
+     *
+     */
+    public void calculerMontantService() throws IOException,
+            NullPointerException {
+        double mntService = 0.0;
+        double tempService = 0.0;
+
+        montantsServ = new double[taille];
+
+        for (int i = 0; i < taille; i++) {
+            switch (idTerrain) {
+                case Terrain.TERRAIN_AGRICOLE: {
+                    mntService = 0.0;
+                }
+                break;
+                case Terrain.TERRAIN_RESIDENTIEL: {
+                    if (superficies[i] <= 500) {
+                        mntService = 0.0;
+                    } else if (superficies[i] > 500
+                            && superficies[i] <= 10000) {
+                        mntService = (2 + services[i]) * 500;
+                    } else {
+                        mntService = (2 + services[i]) * 1000;
+                    }
+                }
+                break;
+                case Terrain.TERRAIN_COMMERCIAL: {
+                    if (superficies[i] <= 500) {
+                        tempService = (2 + services[i]) * 500;
+                    } else if (superficies[i] > 500) {
+                        tempService = (2 + services[i]) * 1500;
+                    }
+
+                    if (tempService > 5000) {
+                        mntService = 5000;
+                    } else {
+                        mntService = tempService;
+                    }
+                }
+                break;
+                default:
+                    break;
+            }
+
+            montantsServ[i] = mntService;
+
+        }
+
+    }
+
+    /**
+     * Calculer le montant par lot.
+     *
+     */
+    public void calculerValeurParLot() throws IOException,
+            NullPointerException {
+        double valeurParLot;
+
+        montantsParLot = new double[taille];
+
+        for (int i = 0; i < taille; i++) {
+            valeurParLot = montantsLot[i] + montantsPass[i] +
+                    montantsServ[i];
+
+            montantsParLot[i] = valeurParLot;
+
+        }
+
+    }
+
+    /**
+     * Calculer la valeur foncière du terrain.
+     *
+     */
+    public void calculerValeurFonciere() throws IOException,
+            NullPointerException {
+        double tempTerrain = 0.0;
+
+        for (int i = 0; i < taille; i++) {
+            tempTerrain = tempTerrain + montantsParLot[i]++;
+        }
+
+        montantTerrain = (tempTerrain + VALEUR_DE_BASE);
+
+    }
+
+    /**
+     * Calculer la taxe scolaire.
+     *
+     */
+    public void calculerTaxeScolaire() throws IOException,
+            NullPointerException {
+        montantTaxeScolaire = montantTerrain * TAXE_SCOLAIRE;
+    }
+
+    /**
+     * Calculer la taxe municipale.
+     *
+     */
+    public void calculerTaxeMunicipale() throws IOException {
+        montantTaxeMunicipale = montantTerrain * TAXE_MUNICIPALE;
+    }
+
+    /**
+     * Afficher le rapport de l'évaluation foncière du terrain.
+     *
+     */
+    public void genererRapportEvaluation()
+            throws IOException {
+        JSONObject enteteJson = new JSONObject();
+        enteteJson.accumulate(RAP_VALEUR_FONCIERE_TOTALE,
+                Utilitaire.convertirMontant(montantTerrain));
+        enteteJson.accumulate(RAP_TAXE_SCOLAIRE,
+                Utilitaire.convertirMontant(montantTaxeScolaire));
+        enteteJson.accumulate(RAP_TAXE_MUNICIPALE,
+                Utilitaire.convertirMontant(montantTaxeMunicipale));
+
+        for (int i = 0; i < montantsParLot.length; i++) {
+            JSONObject lotJson = new JSONObject();
+
+            lotJson.accumulate(DESCRIPTION, descriptions[i]);
+            lotJson.accumulate(RAP_VALEUR_PAR_LOT,
+                    Utilitaire.convertirMontant(montantsLot[i]));
+
+            enteteJson.accumulate(LOTISSEMENTS,lotJson);
+        }
+
+        Utilitaire.afficherMsg(enteteJson.toString(4));
+
+        FileWriterException.saveStringIntoFile(FICHIER_SORTIE,
+                enteteJson.toString(4), FILE_ENCODING);
+    }
+
 }
