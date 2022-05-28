050d94cf6bd3daf7909eacda1eb70c6534bb8ac9
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-28 10:18:10 -0400

    Passage des fichiers d'entrée et sortie comme arguments.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/ProjetDeSessionSprint1.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/ProjetDeSessionSprint1.java
@@ -11,12 +11,11 @@ import java.io.IOException;
 
 
 /**
- 
 * Université du Québec à Montréal (UQAM)
 * Cours : INF2050 - 020 - Été 2022
 * Sprint 1
 *
-*La classe Projet de session contient une methode main.
+* La classe ProjetDeSessionSprint1 contient une methode main.
 * Elle permet de tester toutes les autres classes et les utilisent pour  
 * calculer l'evaluation d'un terrain selon son type pouvant contenir 
 * plusieurs lots. Ces lots sont caratérisés par leur superficie, le nombre de 
@@ -24,6 +23,7 @@ import java.io.IOException;
 * 
 * @author Elza Meguieng Tiemghen
 * @version Mai 2022
+* @modify at 2022-05-28 by Achou Henri Joël / Akaffou
 */
  
 
@@ -34,50 +34,61 @@ public class ProjetDeSessionSprint1 {
      */
     public static void main(String[] args) {
         //Déclaration des variables
+        String entree;
+        String sortie;
         EvaluationTerrain evaluation;
+        
+        if (args.length == 0) {
+            Utilitaire.afficherMsg(
+                    "\nAucune entrée / sortie trouvée.\n");
+        } else {
+            entree = args[0];
+            sortie = args[1];
+        
+            try {
+                // Lire les informations sur le terrain(type de terrain,
+                //les prix max et min, les details du lotissement[description,
+                //les droits de passage, les services, les superficies
+                //et la date de mesure des differents lot])
+                EvaluationTerrain.lireTerrainLoti(entree);
 
-        try {
-            // Lire les informations sur le terrain(type de terrain,
-            //les prix max et min, les details du lotissement[description,
-            //les droits de passage, les services, les superficies
-            //et la date de mesure des differents lot])
-            EvaluationTerrain.lireTerrainLoti();
+                evaluation = new EvaluationTerrain();
 
-            evaluation = new EvaluationTerrain();
+                evaluation.obtenirTypeTerrain();
+                evaluation.obtenirPrixMin();
+                evaluation.obtenirPrixMax();
 
-            evaluation.obtenirTypeTerrain();
-            evaluation.obtenirPrixMin();
-            evaluation.obtenirPrixMax();
+                evaluation.obtenirDescription();
+                evaluation.obtenirNbreDroitPassage();
+                evaluation.obtenirNbreService();
+                evaluation.obtenirSuperficie();
+                evaluation.obtenirDateMesure();
 
-            evaluation.obtenirDescription();
-            evaluation.obtenirNbreDroitPassage();
-            evaluation.obtenirNbreService();
-            evaluation.obtenirSuperficie();
-            evaluation.obtenirDateMesure();
+                //Calculer les différents montants de lots
+                //de droit de passage et montant de service
+                evaluation.calculerMontantLot();
+                evaluation.calculerDroitPassage();
+                evaluation.calculerMontantService();
 
-            //Calculer les différents montants de lots
-            //de droit de passage et montant de service
-            evaluation.calculerMontantLot();
-            evaluation.calculerDroitPassage();
-            evaluation.calculerMontantService();
+                //Calculer les différents montants par lots
+                evaluation.calculerValeurParLot();
 
-            //Calculer les différents montants par lots
-            evaluation.calculerValeurParLot();
+                //Calculer la valeur fonciere et la taxe scolaire
+                //et la taxe municiple qui vont avec
+                evaluation.calculerValeurFonciere();
+                evaluation.calculerTaxeScolaire();
+                evaluation.calculerTaxeMunicipale();
 
-            //Calculer la valeur fonciere et la taxe scolaire
-            //et la taxe municiple qui vont avec
-            evaluation.calculerValeurFonciere();
-            evaluation.calculerTaxeScolaire();
-            evaluation.calculerTaxeMunicipale();
+                //Afficher le rapport de l'évaluation
+                evaluation.genererRapportEvaluation(sortie);
 
-            //Afficher le rapport de l'évaluation
-            evaluation.genererRapportEvaluation();
-
-        } catch (IOException | NullPointerException e) {
-            Utilitaire.afficherMsg("\nAucune entrée / sortie trouvée.\n");
+            } catch (IOException | NullPointerException e) {
+                Utilitaire.afficherMsg(
+                        "\nAucune entrée / sortie trouvée.\n");
+            }
         }
-
     }
     
 }
 
+       
