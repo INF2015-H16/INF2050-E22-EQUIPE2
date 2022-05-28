44f5fd9fa757617fbb29ad5c20a1f7169e59e7bf
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-28 10:20:48 -0400

    Ajout de commentaires.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Utilitaire.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Utilitaire.java
@@ -13,13 +13,25 @@ package inf2050.e22.equipe2;
  *
  * @author Elza Meguieng Tiemghen
  * @version Mai 2022
+ * @modify at 2022-05-28 by Achou Henri Joël / Akaffou
  */
 public class Utilitaire {
     
+    /**
+     * Méthode générique pour faire des affichages
+     * 
+     * @param message qui est le message
+     */
     public static void afficherMsg(String message) {
         System.out.print(message); 
     }
     
+    /**
+     * Arrondir au 5 sous supérieur et formater selon "XX.XX $"
+     * 
+     * @param mont qui est la valeur à formater
+     * @return 
+     */
     public static String convertirMontant(double mont) {
         return String.format("%.2f $", Math.round(Math.ceil(mont *20))/ 20.0);
     }
