16de0dc9b0932e3f00b5dc548817a93a37075f60
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-31 05:11:49 -0400

    Modification du mode d'exception des méthodes  et retrait de certaines méthodes.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/VerificationDonnee.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/VerificationDonnee.java
@@ -4,17 +4,14 @@
  */
 package inf2050.e22.equipe2;
 
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-
 /**
  * Université du Québec à Montréal (UQAM)
  * Cours : INF2050 - 020 - Été 2022
  * Sprint 1
  *
- * La classe VerificationDonnee permet de faire la validation de toutes les données du programme.
- * Elle offre des méthodes pour faire la validation
+ * La classe VerificationDonnee permet de faire
+ * la validation de toutes les données du programme.
+ * Elle offre des méthodes pour faire la validation.
  *
  * @author Aurélien Tcheuffa Kemayou / Sidopz
  * @author Achou Henri Joël / Akaffou
@@ -25,71 +22,94 @@ import java.time.format.DateTimeParseException;
 
 public class VerificationDonnee {
 
-    public static boolean validerTerrain(int type) {
+    private final static int BORNE_INF_TERRAIN = 1;
+    private final static int BORNE_SUP_TERRAIN = 3;
+    
+    /**
+     * Valider l'intervalle dans lequel
+     * doit se situer le type de terrain
+     * 
+     * @param type qui est le type de terrain
+     * @return estValid
+     * @throws IntervallesValideException 
+     */
+     public static boolean validerTerrain(int type)
+            throws IntervallesValideException {
 
-        return type >= 0 && type <= 2; //valide si la valeur est entre 0 et 2 inclusivement
-
-        /** //manière plus explicite de faire la vérification
-        if (type >= 0 && type <= 2){
-            return true;
-        }else {
-            return false;
-        }**/
-
-    }
-
-
-    public static boolean validerLot(String type) {
-
-        //TODO
-        return true;
-
-    }
-
-
-    public static int validerInt(Object valeur) {
-
-        if (valeur instanceof Integer){
-            return (Integer) valeur;
+        boolean estValid = false;
+        
+        if (type < BORNE_INF_TERRAIN || type > BORNE_SUP_TERRAIN) {
+            throw new IntervallesValideException();
         } else {
-            return Integer.parseInt(null);
+            estValid = true;
         }
-
-        //return (valeur instanceof Integer); // si on fait que la méthode retourne un boolean
+        
+        return estValid;
     }
 
-    public static double validerDouble(Object valeur) {
-        if (valeur instanceof Double){
-            return (Double) valeur;
+    /**Verifier si la chaîne de caractères est
+     * une date au format "YYYY-MM-DD"
+     *
+     * @param date est une chaîne de caractères
+     * @return vrai si c'est une date
+     */
+    private static boolean validerDate(String date) {
+        //Déclaration des variables
+        boolean valideDate;
+        int tailleDate;
+        String temp;
+        int tailleTemp;
+        String regex = "((?:19|20)[0-9][0-9])(0?[1-9]|1[012])"
+                + "(0?[1-9]|[12][0-9]|3[01])";
+
+        tailleDate = date.length();
+
+        //Vérifier si la chaîne de caractères contient 10 caractères
+        if (tailleDate != 10) {
+            valideDate = false;
         } else {
-            return Double.parseDouble(null);
+            //Vérifier si les 4ème et 7ème caractères sont des tirets '-'
+            if (date.charAt(4) == '-' && date.charAt(7) == '-') {
+                //Supprimer tous les '-' dans la chaîne de caractères
+                temp = date.replaceAll("[-]", "");
+
+                tailleTemp = temp.length();
+
+                //Vérifier si la chaîne de caractères sans
+                //tiret contient 8 caractères
+                if (tailleTemp != 8) {
+                    valideDate = false;
+                } else {
+                    //Vérifier si chaque caractère de la chaîne est un entier
+                    // respectant l'expression reguliere
+
+                    valideDate = temp.matches(regex);
+                }
+            } else {
+                valideDate = false;
+            }
         }
 
-        //return (valeur instanceof Double); // si on choisit que la méthode retourne un boolean
+        return valideDate;
     }
 
-    public static String validerDateMesure(String date) {
-
-        try {
-            Instant.from(DateTimeFormatter.ISO_INSTANT.parse(date));
-            return date;
-        } catch (DateTimeParseException e) {
-            Utilitaire.afficherMsg("La date ne respecte pas le format ISO 8601");
-        }
-        return null;
-
-        /** // Si cette méthode retourne un boolean:
-        try {
-            Instant.from(DateTimeFormatter.ISO_INSTANT.parse(date));
-            return true;
-        } catch (DateTimeParseException e) {
-            Utilitaire.afficherMsg("La date ne respecte pas le format ISO 8601");
-        }
-        return false;
-         **/
-
+    /**
+     * Valider la date au format 'YYYY-MM-DD'
+     * 
+     * @param date
+     * @return
+     * @throws NumberFormatException 
+     */
+    public static String validerDateMesure(String date)
+            throws NumberFormatException{
+        String dateValid;
+        
+            if (!validerDate(date)) {
+                throw new NumberFormatException();
+            } else {
+                dateValid = date;
+            }
+            
+        return dateValid;
     }
-
-
-
 }
