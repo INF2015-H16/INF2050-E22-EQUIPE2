4e0f2b9f1130f9875d1b6cf8455e541b77970177
akaff <akaff@LAPTOP-TPDTRB7H>
2022-05-31 05:17:33 -0400

    Ajout d'une classe qui gère l'intervalle des possibilités  du type de terrain.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IntervallesValideException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+public class IntervallesValideException extends Exception {
+    
+    public IntervallesValideException () {
+        
+    }
+    
+    public IntervallesValideException (String message) {
+        super(message);  
+    }
+}
