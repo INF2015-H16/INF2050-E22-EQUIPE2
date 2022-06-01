c964cd00f59887e2928f4920102e5c64bf361db1
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-01 08:40:40 -0400

    Ajout classe Exception pour validation lot.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/LotValideException.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 1
+ *
+ * LotValideException : Exception générée lorsque le lot n'est
+ * pas au format "lot N ou s'il y a des doublons".
+ *
+ * @author Achou Henri Joël / Akaffou
+ * @version 19 mai 2022
+ *
+ */
+public class LotValideException extends Exception {
+
+    public LotValideException() {
+    }
+
+    public LotValideException(String message) {
+        super(message);
+    }
+    
+}
