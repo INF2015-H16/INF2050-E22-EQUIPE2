beaaf707d357b5f4fd8e4f7bdf6371a74416040c
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-12 14:24:42 -0400

    Amélioration des méthodes.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Lotissement.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Lotissement.java
@@ -36,40 +36,18 @@ public class Lotissement {
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public int getNombreDroitPassage() {
         return nombreDroitPassage;
-    }
-
-    public void setNombreDroitPassage(int nombreDroitPassage) {
-        this.nombreDroitPassage = nombreDroitPassage;
     }
 
     public int getNombreService() {
         return nombreService;
     }
-
-    public void setNombreService(int nombreService) {
-        this.nombreService = nombreService;
-    }
-
     public int getSuperficie() {
         return superficie;
     }
 
-    public void setSuperficie(int superficie) {
-        this.superficie = superficie;
-    }
-
     public String getDateMesure() {
         return dateMesure;
-    }
-
-    public void setDateMesure(String dateMesure) {
-        this.dateMesure = dateMesure;
     }
 }
