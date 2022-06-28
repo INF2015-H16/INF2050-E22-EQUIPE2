dd31ea5dac4832dc2d72669170e0b1e6d73f0f24
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-28 06:05:12 -0400

    Ajout de la méthode abstraite pour gérer les calculs des montants.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLot.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ *
+ * @author akaff
+ */
+public abstract class MontantLot {
+    // A completer pour le switch des methodes
+    // obtenirMontantLot, obtenirMontantPassage, obtenirMontantService
+}
