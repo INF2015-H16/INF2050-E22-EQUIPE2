57770ccc8d563a6a8764eabc5f2c7983ccf9fa40
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-28 06:08:35 -0400

    Ajout de la méthode enfant pour gérer les calculs des montantsagricoles.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotAgricole.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotAgricole.java
@@ -5,8 +5,16 @@
 package inf2050.e22.equipe2;
 
 /**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 1
  *
- * @author akaff
+ * MontantLotAgricole : Cette classe calcule les montants agricoles.
+ *
+ *
+ * @author Achou Henri Joël / Akaffou
+ * @version 28 juin 2022
+ *
  */
 public class MontantLotAgricole extends MontantLot {
     // A completer pour enfant de MontantLot
