fa24c203328a46b25ac781126c8eab184e879703
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-28 06:03:02 -0400

    Ajout de la classe pour gérer les messages.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/GestionnaireMessage.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ *
+ * @author akaff
+ */
+public class GestionnaireMessage {
+    
+    // STP Elza, Faire ta liste de commentaire ici
+    // Exemple
+    public static final String ERREUR_TYPE_TERRAIN_INFERIEURE
+            = "Le type du terrain est négatif." +
+            " Corriger le SVP !";
+}
