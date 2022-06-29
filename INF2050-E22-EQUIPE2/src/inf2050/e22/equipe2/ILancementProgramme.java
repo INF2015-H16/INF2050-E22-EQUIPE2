b8bbfad1f3c75807cf50c0fd4891230ed139344d
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-29 08:19:21 -0400

    Ajout Interface pour gérer le programme (split de la méthode main).

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/ILancementProgramme.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author akaff
+ */
+public interface ILancementProgramme {
+  
+    void getLesLotissements(String json) throws LotValideException,
+            IntervallesValideException, PrixValideException,
+            LectureFichierException, IOException;
+
+    void getTerrain(String json) throws IntervallesValideException,
+            PrixValideException, LotValideException,
+            LectureFichierException, IOException;
+    void getIdTerrain() throws PrixValideException,
+            IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+
+    void getPrixMinimum() throws PrixValideException,
+            IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+
+    void getPrixMaximum() throws PrixValideException,
+            IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+
+    void getQuantiteLots() throws IntervallesValideException,
+            LotValideException, LectureFichierException,
+            IOException;
+    void getDescriptions() throws IntervallesValideException,
+            LotValideException, LectureFichierException, IOException;
+
+    void getPassages() throws IntervallesValideException,
+            LotValideException, LectureFichierException, IOException;
+
+    void getNombreService() throws IntervallesValideException,
+            LotValideException, LectureFichierException, IOException;
+
+    void getSuperficies()
+            throws IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+
+    void getDates()
+            throws IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+
+   void getMontantsLot() throws IntervallesValideException,
+           LotValideException, LectureFichierException, IOException;
+
+   void getMontantsPassage() throws IntervallesValideException,
+           LotValideException, LectureFichierException, IOException;
+
+   void getMontantsService() throws IntervallesValideException,
+           LotValideException, LectureFichierException, IOException;
+
+    void getMontantsParLot() throws IntervallesValideException,
+            LotValideException, LectureFichierException, IOException;
+
+    void getMontantTerrain()
+            throws IntervallesValideException, LotValideException,
+            LectureFichierException, IOException;
+    void getMontantTaxeScolaire() throws LotValideException,
+            LectureFichierException, IOException;
+    void getMontantTaxeMunicipale() throws LotValideException,
+            LectureFichierException, IOException;
+
+    void rapporterSiBon() throws LotValideException,
+            LectureFichierException, IOException;
+    void afficherRapport() throws LectureFichierException,
+            IOException, LotValideException;
+
+}
