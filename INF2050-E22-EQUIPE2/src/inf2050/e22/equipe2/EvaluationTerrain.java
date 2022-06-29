425b10e38466db4573260d34d6abe1001e26a13a
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-29 08:16:08 -0400

    Split de la méthode pour générer le rapport.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationTerrain.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationTerrain.java
@@ -4,8 +4,7 @@
  */
 package inf2050.e22.equipe2;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
 
@@ -41,88 +40,81 @@ public class EvaluationTerrain implements IEvaluationTerrain {
     public final static String ETIQUETTE_TAXE_SCOLAIRE = "taxe_scolaire";
     public final static String ETIQUETTE_TAXE_MUNICIPALE = "taxe_municipale";
     public final static String ETIQUETTE_VALEUR_PAR_LOT = "valeur_par_lot";
-
-    public final static String DECIMAL_ONLY = "[^\\d.]";
+    public final static String DECIMAL_SEULEMENT_POINT = "[^\\d.]";
+    public static final String MESSAGE = "message";
 
     @Override
     public Terrain obtenirDonneesTerrain(String json,
-            EvaluationLot lotissements)
-            throws FileNotFoundException, IOException,
-            NumberFormatException, IntervallesValideException,
-            PrixValideException {
-
+                                         EvaluationLot lotissements)
+            throws IntervallesValideException, PrixValideException, JSONException {
         Terrain terrain = null;
         if (json.length() != 0) {
             JSONObject enteteTerrain = JSONObject.fromObject(json);
-            
-            int typeTerrain = VerificationDonnee
-                    .valeurEstInt(enteteTerrain
-                            .getInt(ETIQUETTE_TYPE_TERRAIN));
-            String prixMin = VerificationDonnee.
-                    validerPrix(enteteTerrain
-                            .getString(ETIQUETTE_PRIX_M2_MIN));
-            String priMax = VerificationDonnee.
-                    validerPrix(enteteTerrain
-                            .getString(ETIQUETTE_PRIX_M2_MAX));
-            
-            if (VerificationDonnee.validerTypeTerrain(typeTerrain)) {
-                terrain = new Terrain(typeTerrain, prixMin,
-                        priMax, lotissements);
-            }
-        }
-        
-        return terrain;
 
+            int typeTerrain = VerificationDonnee
+                    .validerTypeTerrain(enteteTerrain
+                            .getInt(ETIQUETTE_TYPE_TERRAIN));
+            String prixMin = VerificationDonnee.validerPrix
+                                    (VerificationDonnee
+                                            .verifierPrixNegatif(enteteTerrain
+                            .getString(ETIQUETTE_PRIX_M2_MIN)));
+            String priMax = VerificationDonnee.validerPrix
+                    (VerificationDonnee.verifierPrixNegatif(enteteTerrain
+                            .getString(ETIQUETTE_PRIX_M2_MAX)));
+
+            terrain = new Terrain(typeTerrain, prixMin,
+                    priMax, lotissements);
+
+        }
+
+        return terrain;
     }
 
     @Override
     public int obtenirTypeTerrain(Terrain terrain)
-            throws IOException, NullPointerException {
+            throws NullPointerException {
         return terrain.getTypeTerrain();
     }
 
     @Override
     public double obtenirPrixMinimum(Terrain terrain)
-            throws IOException, NullPointerException {
-        return Double.parseDouble(terrain.getPrixMin()
-                .replaceAll(",",".")
-                .replaceAll(DECIMAL_ONLY, ""));
+            throws NullPointerException, PrixValideException {
+        String temp = terrain.getPrixMin();
+        String changerVirgule = temp.replaceAll(",", ".");
+        String prixMinimum = changerVirgule
+                .replaceAll(DECIMAL_SEULEMENT_POINT, "");
+        return Double.parseDouble(prixMinimum);
     }
 
     @Override
     public double obtenirPrixMaximum(Terrain terrain)
-            throws IOException, NullPointerException {
-        return Double.parseDouble(terrain.getPrixMax()
-                .replaceAll(",",".")
-                .replaceAll(DECIMAL_ONLY, ""));
+            throws NullPointerException, PrixValideException {
+        String temp = terrain.getPrixMax();
+        String changerVirgule = temp.replaceAll(",", ".");
+        String prixMaximum = changerVirgule
+                .replaceAll(DECIMAL_SEULEMENT_POINT, "");
+        return Double.parseDouble(prixMaximum);
     }
+
 
     @Override
     public double calculerTaxeScolaire(double montantTerrain)
-            throws IOException, NullPointerException {
-        
+            throws NullPointerException {
         return montantTerrain * TAXE_SCOLAIRE;
     }
 
     @Override
-    public double calculerTaxeMunicipale(double montantTerrain)
-            throws IOException {
-        
+    public double calculerTaxeMunicipale(double montantTerrain) {
         return montantTerrain * TAXE_MUNICIPALE;
     }
 
     @Override
-    public void genererRapportEvaluation (String sortie, double montantTerrain,
-                       double montantTaxeScolaire, double montantTaxeMunicipale,
-                       double [] montantsParLot, String [] descriptions)
-            throws IOException, LotValideException {
-        // Methode à completer
-    }
-
     public JSONObject fournirRapportValide(double montantTerrain,
-            double montantTaxeScolaire, double montantTaxeMunicipale,
-            double [] montantsParLot, String [] descriptions)
-            throws IOException, LotValideException {
+                                           double montantTaxeScolaire,
+                                           double montantTaxeMunicipale,
+                                           double [] montantsParLot,
+                                           String [] descriptions)
+            throws LotValideException {
 
         JSONObject donneRapport = new JSONObject();
         if (VerificationDonnee.verifierLotsDoublons(descriptions)) {
@@ -167,6 +159,13 @@ public class EvaluationTerrain implements IEvaluationTerrain {
                 Utilitaire.convertirMontant(montantsParLot[index]));
 
         return detailsLot;
+    }
+
+    @Override
+    public JSONObject fournirRapportErreur(String message) {
+        JSONObject rapport = new JSONObject();
+
+        return rapport.accumulate(MESSAGE, message);
     }
 
 }
