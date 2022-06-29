48bb8e919e7eefd2353ce2e976071913d4872bf6
akaff <akaff@LAPTOP-TPDTRB7H>
2022-06-29 07:06:01 -0400

    Dispatching du switch pour le calcul des montants lot.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotResidentiel.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotResidentiel.java
@@ -9,5 +9,50 @@ package inf2050.e22.equipe2;
  * @author akaff
  */
 public class MontantLotResidentiel extends MontantLot {
-    // A completer pour enfant de MontantLot
+
+    public MontantLotResidentiel() {
+        super(MontantLot.TERRAIN_RESIDENTIEL);
+    }
+
+    @Override
+    public double obtenirMontantLot(int index,
+                                    int[] superficies, double prixMinimum,
+                                    double prixMaximum) {
+        return superficies[index] * ((prixMinimum + prixMaximum)/2);
+    }
+
+    @Override
+    public double obtenirMontantPassage(int index,
+                                        int[] passages, double[] montantsLot) {
+        return MONTANT_BASE_DROIT_PASSAGE
+                - (passages[index]
+                * (TAUX_10 * montantsLot[index]));
+    }
+
+    @Override
+    public double obtenirMontantService(int index,
+                                        int[] superficies, int[] services) {
+        return evaluerTerrainResidentiel(index,
+                superficies, services);
+    }
+
+    private double evaluerTerrainResidentiel(int index, int [] superficies,
+                                            int [] services) {
+        double service;
+
+        if (superficies[index] <= SUPERFICIE_500) {
+            service = MONTANT_SERVICE_0;
+        } else if (superficies[index] > SUPERFICIE_500
+                && superficies[index] <= SUPERFICIE_10000) {
+            service = (SERVICE_DE_BASE
+                    + services[index]) * MONTANT_SERVICE_500;
+        } else {
+            service = (SERVICE_DE_BASE
+                    + services[index]) * MONTANT_SERVICE_1000;
+        }
+
+        return service;
+
+    }
+
 }
