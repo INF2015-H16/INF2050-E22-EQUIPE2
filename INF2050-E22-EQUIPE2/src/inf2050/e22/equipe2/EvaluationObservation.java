c33f033d434d7ff0b9ce99cac097d629653ba7c3
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-17 05:15:46 -0400

    Creation de la classe pour gérer les observations.

---
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationObservation.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inf2050.e22.equipe2;
+
+/**
+ *
+ * @author akaff
+ */
+public class EvaluationObservation {
+    
+}
