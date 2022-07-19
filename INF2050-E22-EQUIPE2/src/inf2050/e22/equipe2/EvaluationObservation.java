fd7ce0eb04a7968136ac90e5f51f8cb17d269541
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-18 22:56:25 -0400

    Redaction des observations à utiliser.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationObservation.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/EvaluationObservation.java
@@ -10,4 +10,18 @@ package inf2050.e22.equipe2;
  */
 public class EvaluationObservation {
     
+    public static final String LA_VALEUR_PAR_LOT_DU_LOT = "La valeur par lot du lot ";
+    public static final String EST_TROP_DISPENDIEUSE = " est trop dispendieuse.";
+    public static final String MSG_DLE = "La taxe municipale payable par le propriétaire nécessite deux " +
+            "versements.";
+    public static final String MSG_DATE = "L’écart maximal entre les dates de mesure des lots d’un même " +
+            "terrain devrait être de moins de 6 mois.";
+    public static final String MSG = "La taxe scolaire payable par le propriétaire nécessite deux " +
+            "versements.";
+    public static final String VALEUR_FONCIÈRE_TOTALE_NE_DOIT_PAS_DÉPASSER_300000
+            = "La valeur foncière totale ne doit pas dépasser 300000 $";
+    public static final String LA_SUPERFFICIE_DU_LOT = "La superfficie du lot ";
+    public static final String EST_TROP_GRANDE = " est trop grande.";
+    public static final String PAS_DÉPASSER_DEUX_FOIS_LE_PRIX_MINIMUM_DU_M_2
+            = "Le prix maximum du m2 ne peut pas dépasser deux fois le prix minimum du m2.";
 }
