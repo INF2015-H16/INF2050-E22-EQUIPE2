94c2e5740762eb9aebfc31b23519c239533bdbfc
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-19 09:53:14 -0400

    Ajout de méthode à la classe EvaluationObservation.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IObservationTerrain.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IObservationTerrain.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package inf2050.e22.equipe2;
 
 /**
