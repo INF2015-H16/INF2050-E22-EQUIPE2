643ad2f68510b7467ef6ca2456e70f078b48abbb
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-22 07:43:44 -0400

    Amélioration de la JavaDoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotAgricole.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotAgricole.java
@@ -7,13 +7,13 @@ package inf2050.e22.equipe2;
 /**
  * Université du Québec à Montréal (UQAM)
  * Cours : INF2050 - 020 - Été 2022
- * Sprint 1
+ * Sprint 3
  *
  * MontantLotAgricole : Cette classe calcule les montants agricoles.
  *
  *
  * @author Achou Henri Joël / Akaffou
- * @version 28 juin 2022
+ * @version juillet 2022
  *
  */
 public class MontantLotAgricole extends MontantLot {
