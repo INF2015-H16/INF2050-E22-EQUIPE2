11bede495eed6b0d098172c43e77dbda822f6c2b
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-22 07:44:19 -0400

    Amélioration de la JavaDoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/StatistiquePresenter.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/StatistiquePresenter.java
@@ -3,18 +3,18 @@ package inf2050.e22.equipe2;
 import java.io.IOException;
 
 /**
- *  *  * Université du Québec à Montréal (UQAM)
- *  *  * Cours : INF2050 - 020 - Été 2022
- *  *  * Sprint 3
- *  *
- *  *  *
- *  *   @author Jordan Mone
- *  *  * @version 20 Juillet 2022
- *  *  *
- *  *  */
-
-
-
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 3
+ *
+ * Statistique : Cette classe contient les données
+ *  *            sur la gestion des statistiques.
+ *
+ *
+ * @author Jordan Mone
+ * @version juillet 2022
+ *
+ */
 public class StatistiquePresenter {
 
     private IStatistique iStatistique;
