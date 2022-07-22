b13bf007ed47ba07400ee38e06f30945111dc1da
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-22 07:41:54 -0400

    Amélioration JavaDoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IRapportStatistique.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IRapportStatistique.java
@@ -6,21 +6,20 @@ import java.util.ArrayList;
 
 
 /**
- *  * Université du Québec à Montréal (UQAM)
- *  * Cours : INF2050 - 020 - Été 2022
- *  * Sprint 3
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 3
  *
- *  *
- *   @author Jordan Mone
- *  * @version 20 Juillet 2022
- *  *
- *  */
-
+ * IRapportStatistique : Cette interface contient les données
+ *                       sur la gestion des statistiques.
+ *
+ *
+ * @author Jordan Mone
+ * @version juillet 2022
+ *
+ */
 public interface IRapportStatistique {
 
-    double obtenirMontantRepartiParLot(double [] montantsParLots,
-                                       ArrayList<Lotissement> lotissements)
-            throws IntervallesValideException;
     void genererRapportStat(File file, String content)
             throws IOException;
     ArrayList<Statistique> lireFichierStatistiques()
