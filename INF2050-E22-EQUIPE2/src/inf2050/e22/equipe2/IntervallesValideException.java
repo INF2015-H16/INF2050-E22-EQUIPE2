33698353356cc0ea86639c2f0ddf83c6762a9388
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-22 07:42:10 -0400

    Amélioration JavaDoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IntervallesValideException.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IntervallesValideException.java
@@ -4,6 +4,20 @@
  */
 package inf2050.e22.equipe2;
 
+/**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 3
+ *
+ * IntervallesValideException : Cette classe permet de gérer les erreurs
+ *                      que fournissent une valeur qui est en déhors d'un
+ *                      certain intervalle donné.
+ *
+ *
+ * @author Achou Henri Joël / Akaffou
+ * @version juillet 2022
+ *
+ */
 public class IntervallesValideException extends Exception {
 
     public IntervallesValideException () {
