15590bc1f39549444d2da38a29eeaf4c9f5e66c2
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-22 07:43:49 -0400

    Amélioration de la JavaDoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotCommercial.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/MontantLotCommercial.java
@@ -5,8 +5,16 @@
 package inf2050.e22.equipe2;
 
 /**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 3
  *
- * @author akaff
+ * MontantLotAgricole : Cette classe calcule les montants commerciaux.
+ *
+ *
+ * @author Achou Henri Joël / Akaffou
+ * @version juillet 2022
+ *
  */
 public class MontantLotCommercial extends MontantLot {
 
