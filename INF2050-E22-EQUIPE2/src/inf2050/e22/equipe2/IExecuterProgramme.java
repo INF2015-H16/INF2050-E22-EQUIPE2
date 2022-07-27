b5625020c040ce08f48106f23b8a98497da72a1c
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-27 09:24:25 -0400

    Correction parametres ligne de commande.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IExecuterProgramme.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IExecuterProgramme.java
@@ -2,7 +2,7 @@ package inf2050.e22.equipe2;
 
 public interface IExecuterProgramme {
 
-    void lancerProgramme();
+    void lancerProgramme(String entree, String Sortie);
     void rapporterStatistique();
     void reinitialiserStatistique();
 }
