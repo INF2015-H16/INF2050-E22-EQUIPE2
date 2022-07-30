bb4c2f0730678649ccd66fbfe539a84c7a401157
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-30 06:29:41 -0400

    Ajout javadoc.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IExecuterProgramme.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IExecuterProgramme.java
@@ -1,5 +1,20 @@
 package inf2050.e22.equipe2;
 
+/**
+ * Université du Québec à Montréal (UQAM)
+ * Cours : INF2050 - 020 - Été 2022
+ * Sprint 3
+ *
+ * IExecuterProgramme : Cette interface se comporte à peu près comme un menu.
+ *                      Elle offre trois choix, à savoir lancer le programme,
+ *                      fournir le rapport statistique et réinitialiser
+ *                      le programme.
+ *
+ *
+ * @author Elza Meguieng Tiemghen
+ * @version juillet 2022
+ *
+ */
 public interface IExecuterProgramme {
 
     void lancerProgramme(String entree, String Sortie);
