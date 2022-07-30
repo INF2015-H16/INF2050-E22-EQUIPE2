ffa747cd89be16c2535bc72dd8d7e68f5ae63309
akaff <akaff@LAPTOP-TPDTRB7H>
2022-07-30 14:55:01 -0400

    Modification methode.

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IEvaluationTerrain.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/IEvaluationTerrain.java
@@ -54,6 +54,4 @@ public interface IEvaluationTerrain {
                                     ArrayList<String> observationChainee)
             throws IOException, LotValideException, LectureFichierException;
 
-    JSONObject fournirRapportErreur(String message);
-
 }
