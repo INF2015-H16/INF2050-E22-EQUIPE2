5fa1cf265fb289e782be94a990013c2db34e03b1
kemayou <akemayou@gmail.com>
2022-08-01 15:40:51 -0400

    Mise à jour du mode d'arrondissement

--- INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Utilitaire.java
+++ INF2050-E22-EQUIPE2/src/inf2050/e22/equipe2/Utilitaire.java
@@ -4,12 +4,15 @@
  */
 package inf2050.e22.equipe2;
 
+import java.math.*;
+import static java.math.RoundingMode.*;
+
 /**
  * Université du Québec à Montréal (UQAM)
  * Cours : INF2050 - 020 - Été 2022
  * Sprint 3
  *
- * La classe utilitaire contient des methodes utiles
+ * La classe utilitaire contient des méthodes utiles
  *
  * @author Elza Meguieng Tiemghen
  * @version juillet 2022
@@ -20,13 +23,39 @@ public class Utilitaire {
     public static void afficherMessage(String message) {
         System.out.print(message); 
     }
-    
+
     /**
-     * Arrondir au 5 sous et formater selon "XX.XX $"
+     * Détermine s'il faut arrondir au 5c supérieur ou bien inférieur
+     */
+    private static boolean determinerTypeArrondi(double montant){
+        String montantAValider = String.format("%.2f", montant);
+        return (montantAValider.charAt(montantAValider.length() - 1) >= '5');
+    }
+
+    /**
+     * Arrondir au 5 sous supérieur ou inférieur et formater selon "XX.XX $"
      */
     public static String convertirMontant(double montant) {
-        return String.format(
-                "%.2f $", Math.round(montant*20)/20.0) ;
+        BigDecimal montantAConvertir = new BigDecimal(montant);
+
+        if (determinerTypeArrondi(montant)){
+            return String.format(
+                    "%.2f $", round(montantAConvertir, montantAConvertir,HALF_UP));
+        } else {
+            return String.format(
+                    "%.2f $", round(montantAConvertir, montantAConvertir,HALF_DOWN));
+        }
+    }
+
+    public static BigDecimal round(BigDecimal value, BigDecimal increment,
+                                   RoundingMode roundingMode) {
+        if (increment.signum() == 0) {
+            return value;
+        } else {
+            BigDecimal divided = value.divide(increment, 0, roundingMode);
+            BigDecimal result = divided.multiply(increment);
+            return result;
+        }
     }
 
     public static String afficherFormatRapport(String libelle, int valeur) {
